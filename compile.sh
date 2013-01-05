8818f1d0d25d375544cd9e110c4a95f89673b6de
Marzy-ya <marzy.ya@gmail.com>
2013-01-05 17:12:03 +0800

    Add a linker for fedora

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Author:yangxh92@gmail.com
-g++ display.cpp Sources/cal_point.cpp -o display -lGLU -lglut -std=c++0x
+g++ display.cpp Sources/cal_point.cpp -o display -lGL -lGLU -lglut -std=c++0x
